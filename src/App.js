250ee93522c35eb1552a527dad092ddb5c9f1af2
manu <manu.villar.vitoria@gmail.com>
2020-05-21 18:10:34 +0200

    conditional load of components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useState } from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
+import Mensaje from './componentes/Mensaje';
+import Resultado from './componentes/Resultado';
 
 
 function App() {
@@ -11,6 +13,14 @@ function App() {
   const [cantidad, guardarCantidad] = useState(0);
   const [plazo, guardarPlazo] = useState('');
   const [total, guardarTotal] = useState(0);
+
+  let componente;
+
+  if(total === 0) {
+    componente = <Mensaje />
+  } else {
+    componente = <Resultado />
+  }
 
   return (
     <Fragment>
@@ -26,9 +36,9 @@ function App() {
           total = {total} 
           guardarTotal = {guardarTotal} 
         />
-
-        <p>Total a pagar: ${total}</p>
-        
+        <div className="mensajes">
+          {componente}
+        </div>
       </div>
     </Fragment>
   );
---
+++ src/componentes/Resultado.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Resultado = () => {
+    return (  
+        <h1>desde resultado</h1>
+    );
+}
+ 
+export default Resultado;
