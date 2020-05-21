f30f7b1cf11668287980031fa8d775cb4c6adfde
manu <manu.villar.vitoria@gmail.com>
2020-05-21 14:13:13 +0200

    using arrow function to the component with props

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,22 +1,6 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
-
-//Creamos el componente Header
-//function Header(props) {
-  function Header({titulo}) { // Hacemos destructuring
-
-     console.log(titulo);
-
-    //Este return en React imprime el html en el template.
-    //Cuando hay más de un valor dentro de return, hay que poner Fragment o <div className="App">
-    //Si queremos colocar código JS dentro del JSX, debemos colocarlo entre llaves.
-    return(
-        <Fragment> 
-       {/* <h1>{props.titulo}</h1> */}
-       {/* <p>{props.descripcion}</p> */}
-           <h1>{titulo}</h1>
-        </Fragment>
-    )
-}
+// Usando 'sfc' de react snippets se crea el cuerpo de la  arrow function
+const Header = ({titulo}) => <h1>{titulo}</h1>;
 
 export default Header;
